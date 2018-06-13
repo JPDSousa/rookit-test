a6867a42e8995f07f02c3409dfa5fa5fbf7e9bf8
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2018-06-13 08:06:39 +0100

    More test categories

--- src/main/java/org/rookit/test/categories/FastTest.java
+++ src/main/java/org/rookit/test/categories/FastTest.java
@@ -21,8 +21,18 @@
  ******************************************************************************/
 package org.rookit.test.categories;
 
+import org.junit.jupiter.api.Tag;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 @SuppressWarnings("javadoc")
-public interface FastTest {
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("fast")
+public @interface FastTest {
 
     // used as JUnit category
     
--- src/main/java/org/rookit/test/categories/UnitTest.java
+++ src/main/java/org/rookit/test/categories/UnitTest.java
@@ -21,8 +21,18 @@
  ******************************************************************************/
 package org.rookit.test.categories;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.junit.jupiter.api.Tag;
+
 @SuppressWarnings("javadoc")
-public interface UnitTest {
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("fast")
+public @interface UnitTest {
 
     // Used as a JUnit category
     
