be7bec159d37e82b702019436d47a6fdeea40a86
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2018-06-13 08:07:51 +0100

    Refactor

--- src/test/java/org/rookit/test/generator/AbstractGeneratorTest.java
+++ src/test/java/org/rookit/test/generator/AbstractGeneratorTest.java
@@ -24,10 +24,10 @@ package org.rookit.test.generator;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
-import org.rookit.test.AbstractTest;
+import org.rookit.test.AbstractUnitTest;
 import org.rookit.test.SingletonTest;
 
-abstract class AbstractGeneratorTest<T extends Generator<?>> extends AbstractTest<T> implements SingletonTest<T> {
+abstract class AbstractGeneratorTest<T extends Generator<?>> extends AbstractUnitTest<T> implements SingletonTest<T> {
 
     protected static final short CONFIDENCE_THRESHOLD = 10;
     private static final Injector INJECTOR = Guice.createInjector(new BaseGeneratorModule());
